d4809e7e4033ada569ca82c0d188d99a58a0834c
NextLegacy <mcserezli@gmail.com>
2025-03-28 18:46:55 +0100

    test

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,8 @@
-import { env } from "$env/dynamic/private";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (!Deno.env.get("DATABASE_URL")) throw new Error("DATABASE_URL is not set");
 
-const client = neon(env.DATABASE_URL);
+const client = neon(Deno.env.get("DATABASE_URL"));
 
 export const db = drizzle(client);
