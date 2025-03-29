d31ec4a3a6d33601caae43585fd4d3426bc203a9
NextLegacy <mcserezli@gmail.com>
2025-03-29 04:01:26 +0100

    last one for tonight...

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -8,7 +8,13 @@ console.log(process.env["DATABASE_URL"]);
 console.log(process.env.DATABASE_URL);
 console.log(import.meta.env.DATABASE_URL);
 
-const DATABASE_URL = env["DATABASE_URL"];
+console.log(Deno.env.get("VITE_DATABASE_URL"));
+console.log(env["VITE_DATABASE_URL"]);
+console.log(process.env["VITE_DATABASE_URL"]);
+console.log(process.env.VITE_DATABASE_URL);
+console.log(import.meta.env.VITE_DATABASE_URL);
+
+const DATABASE_URL = env["VITE_DATABASE_URL"];
 
 if (!DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
