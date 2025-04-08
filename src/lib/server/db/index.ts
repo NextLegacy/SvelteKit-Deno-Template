42149b51d6c3a3b7e761c555934d27e05952ccef
NextLegacy <mcserezli@gmail.com>
2025-04-08 19:28:02 +0200

    hack

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,6 +4,13 @@ import { neon } from "@neondatabase/serverless";
 
 import { env } from "$env/dynamic/private";
 
+await new Promise((resolve) => {
+    while (!env.DATABASE_URL) {
+        setTimeout(resolve, 10);
+    }
+    resolve(true);
+});
+
 const client = neon(env.DATABASE_URL);
 
 export const db = drizzle(client);
