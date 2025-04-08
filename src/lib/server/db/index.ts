df2ef800d71b2f1714b106cb7113a132bc47281e
NextLegacy <mcserezli@gmail.com>
2025-04-08 19:14:49 +0200

    test

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,10 +4,8 @@ import { neon } from "@neondatabase/serverless";
 
 import { env } from "$env/dynamic/private";
 
-const DATABASE_URL = env.DATABASE_URL;
+export const DATABASE_URL = env.DATABASE_URL;
 
-if (!DATABASE_URL) throw new Error("DATABASE_URL is not set");
-
-const client = neon(DATABASE_URL);
+export const client = neon(DATABASE_URL);
 
 export const db = drizzle(client);
